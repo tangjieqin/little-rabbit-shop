d885dfebafd0d288501d5cb9d093664b77209b67
jaclyn_tang <jaclyn_tang@163.com>
2026-09-05 18:01:44 +0800

    首页-自定义导航栏

--- mp-weixin-uniapp/src/pages/index/index.vue
+++ mp-weixin-uniapp/src/pages/index/index.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
+import CustomNavbar from './componets/CustomNavbar.vue'
+
 //
 </script>
 
 <template>
+  <custom-navbar />
   <view class="index">index</view>
-  <uni-card title="基础卡片" extra="额外信息">
-    <text>这是一个基础卡片示例，此示例展示了一个标题加标题额外信息的标准卡片。</text>
-  </uni-card>
 </template>
 
 <style lang="scss" scoped>
