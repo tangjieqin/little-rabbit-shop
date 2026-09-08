a1ddcd46c645e85c8f2bb8e673b4d15b6a942885
jaclyn_tang <jaclyn_tang@163.com>
2026-09-08 21:17:35 +0800

    首页分类

--- mp-weixin-uniapp/src/pages/index/index.vue
+++ mp-weixin-uniapp/src/pages/index/index.vue
@@ -1,16 +1,21 @@
 <template>
+  <!-- 自定义导航栏 -->
   <CustomNavbar />
+  <!-- 自定义轮播图 -->
   <XtxSwiper :list="bannerList" />
+  <!-- 分类面板 -->
+  <CategoryPanel :list="categoryList" />
 
   <view class="index">index</view>
 </template>
 
 <script setup lang="ts">
 import CustomNavbar from '@/pages/index/components/CustomNavbar.vue'
-import { getHomeBannerAPI } from '@/services/home'
-import type { BannerItem } from '@/types/home'
+import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
+import type { BannerItem, CategoryItem } from '@/types/home'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
+import CategoryPanel from '@/pages/index/components/CategoryPanel.vue'
 
 // 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
@@ -19,10 +24,23 @@ const getHomeBannerData = async () => {
   bannerList.value = res.result
 }
 
+// 获取前台页面类目数据
+const categoryList = ref<CategoryItem[]>([])
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  categoryList.value = res.result
+}
+
 // 页面加载
 onLoad(() => {
   getHomeBannerData()
+  getHomeCategoryData()
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+/* 主界面的颜色和底部区分开来 */
+page {
+  background-color: #f7f7f7;
+}
+</style>
