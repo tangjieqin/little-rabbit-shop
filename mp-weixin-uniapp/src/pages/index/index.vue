2fed1cc5e2055e077ad497303c7d890aff9ea390
jaclyn_tang <jaclyn_tang@163.com>
2026-09-09 21:16:49 +0800

    首页-猜你喜欢推荐

--- mp-weixin-uniapp/src/pages/index/index.vue
+++ mp-weixin-uniapp/src/pages/index/index.vue
@@ -1,14 +1,17 @@
 <template>
   <!-- 自定义导航栏 -->
   <CustomNavbar />
-  <!-- 自定义轮播图 -->
-  <XtxSwiper :list="bannerList" />
-  <!-- 分类面板 -->
-  <CategoryPanel :list="categoryList" />
-  <!-- 热门推荐 -->
-  <HotPanel :list="hotList" />
-
-  <view class="index">index</view>
+  <!-- 保持自定义导航栏不动，其他的可以上下滑动 -->
+  <scroll-view @scrolltolower="onScrolttolower" class="scroll-view" scroll-y>
+    <!-- 自定义轮播图 -->
+    <XtxSwiper :list="bannerList" />
+    <!-- 分类面板 -->
+    <CategoryPanel :list="categoryList" />
+    <!-- 热门推荐 -->
+    <HotPanel :list="hotList" />
+    <!-- 猜你喜欢 -->
+    <XtxGuess ref="xtxGuessRef" />
+  </scroll-view>
 </template>
 
 <script setup lang="ts">
@@ -19,6 +22,7 @@ import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 import CategoryPanel from '@/pages/index/components/CategoryPanel.vue'
 import HotPanel from '@/pages/index/components/HotPanel.vue'
+import type { XtxGuessInstance } from '@/components'
 
 // 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
@@ -47,11 +51,27 @@ onLoad(() => {
   getHomeCategoryData()
   getHomeHotData()
 })
+
+// 获取猜你喜欢组件实例
+const xtxGuessRef = ref<XtxGuessInstance>()
+
+// 滚动到底部加载更多数据
+const onScrolttolower = () => {
+  xtxGuessRef.value?.getMore()
+}
 </script>
 
 <style lang="scss">
 /* 主界面的颜色和底部区分开来 */
 page {
   background-color: #f7f7f7;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
+.scroll-view {
+  flex: 1;
+  overflow: hidden;
 }
 </style>
--- mp-weixin-uniapp/src/services/home.ts
+++ mp-weixin-uniapp/src/services/home.ts
@@ -1,4 +1,5 @@
-import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
+import type { PageParams, PageResult } from '@/types/global'
+import type { GuessItem, BannerItem, CategoryItem, HotItem } from '@/types/home'
 import { http } from '@/utils/http'
 
 /**
@@ -34,5 +35,17 @@ export const getHomeHotAPI = () => {
   return http<HotItem[]>({
     method: 'GET',
     url: '/home/hot/mutli',
+  })
+}
+
+/**
+ * 猜你喜欢-小程序
+ * @returns
+ */
+export const getHomeGoodsGuessLikeAPI = (data?: PageParams) => {
+  return http<PageResult<GuessItem>>({
+    method: 'GET',
+    url: '/home/goods/guessLike',
+    data,
   })
 }
